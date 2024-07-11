3b72dbc04e6a1fa438ab435ee186f5a35c03e985
Esmaeil <esmaeil.changizy@gmail.com>
2024-07-10 20:59:51 -0400

    Update bank.js

--- bank.js
+++ bank.js
@@ -198,6 +198,7 @@ app.post("/Saving.ejs", function (req, res) {
     }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running on port 3000");
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
